bc8cb876a74d46a525a0dcef0cdf638341124985
JonnatasCabral <matiasjonnatas@gmail.com>
2015-06-21 22:55:17 -0300

    alteração no contexto da pagina

--- FCAPJr/fcap_app/admin.py
+++ FCAPJr/fcap_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import (Banner, Area, Membro)
 
-# Register your models here.
+admin.site.register([Banner, Area, Membro])
+
--- FCAPJr/fcap_app/models.py
+++ FCAPJr/fcap_app/models.py
@@ -1,5 +1,45 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+# Banner (img,descricao)
+# Area de membros (nome da area e membros)
+# membro (nome, cargo)
 
-#Banner (img,descricao)
-#Area de membros (nome da area e membros)
-#membro (nome, cargo)
+
+class Banner(models.Model):
+
+    imagem = models.ImageField('Imagem')
+    descricao = models.TextField('Descricao', max_length=255,
+                                 blank=True, null=True)
+
+    def __unicode__(self):
+        pass
+
+
+class Area(models.Model):
+
+    nome = models.CharField('nome da area', max_length=255)
+
+    class Meta:
+        verbose_name = "Area"
+        verbose_name_plural = "Areas"
+
+    def __unicode__(self):
+        pass
+
+
+class Membro(models.Model):
+
+    area = models.ForeignKey(Area)
+
+    nome = models.CharField('nome', max_length=255,
+                            blank=False, null=False)
+    Cargo = models.CharField('cargo', max_length=255,
+                             blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Membro"
+        verbose_name_plural = "Membros"
+
+    def __unicode__(self):
+        pass
+
