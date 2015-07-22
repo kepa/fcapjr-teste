4533b11ff1543af50a3a68575e48db9c1fac3e46
leoschet <leoschettini2@gmail.com>
2015-07-22 11:11:19 -0300

    Integrar models de MVV com front

--- FCAPJr/fcap_app/views.py
+++ FCAPJr/fcap_app/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
-from .models import Banner
+from django.views.generic import ListView, TemplateView
+from .models import Banner, Mission, Vision, Value
 
 
-class BannersView(ListView):
-    model = Banner
+class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def banners(self):
+    	return Banner.objects.all()
+
+    def mission(self):
+    	return Mission.objects.all()[0]
+
+    def vision(self):
+    	return Vision.objects.all()[0]
+
+    def values(self):
+    	return Value.objects.all()[0]
 
     # def get_queryset(self):
     #     queryset = super(BannersView, self).get_queryset()
     #     return queryset
 
 
-home = BannersView.as_view()
+home = IndexView.as_view()
