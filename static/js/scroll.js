74bcf7a140e77aca3ca885163ca4b11e577d3758
KabuVerdi <edyetn@gmail.com>
2015-07-31 03:25:53 -0100

    caroussel

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -7,18 +7,26 @@ $("#quensomos").click(function(){
 });
 
 $("#equipe").click(function(){
-	$("html, body").animate({ scrollTop: $('#team').offset().top -60 }, 700);
+	$("html, body").animate({ scrollTop: $('#team').offset().top -90 }, 700);
 });
 
 $("#events").click(function(){
-	$("html, body").animate({ scrollTop: $('#evento').offset().top -60 }, 700);
+	$("html, body").animate({ scrollTop: $('#eventos').offset().top -60 }, 700);
+});
+
+$("#portfolio").click(function(){
+	$("html, body").animate({ scrollTop: $('#eventos').offset().top -60 }, 700);
 });
 
 $("#servicess").click(function(){
-	$("html, body").animate({ scrollTop: $('#servi').offset().top -100 }, 700);
+	$("html, body").animate({ scrollTop: $('#servi').offset().top -70 }, 700);
 });
 
 // Pagina do curso:
 $("#contat").click(function(){
+	$("html, body").animate({ scrollTop: $('#contato').offset().top -100 }, 500);
+});
+
+$("#contatolink").click(function(){
 	$("html, body").animate({ scrollTop: $('#contato').offset().top -100 }, 500);
 });
