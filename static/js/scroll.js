c53c9761052f3fc3e0e2f23f0491b10c3d802605
leoschet <leoschettini2@gmail.com>
2015-08-08 16:17:03 -0300

    fundo de doacao + ajustes menu + ajustes gerais

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -3,23 +3,23 @@ $("#homes").click(function(){
 });
 
 $("#quensomos").click(function(){
-	$("html, body").animate({ scrollTop: $('#quemSomos').offset().top -90 }, 700);
+	$("html, body").animate({ scrollTop: $('#quemSomos').offset().top -90 }, 500);
 });
 
 $("#equipe").click(function(){
-	$("html, body").animate({ scrollTop: $('#equipeSection').offset().top -82 }, 700);
+	$("html, body").animate({ scrollTop: $('#equipeSection').offset().top -82 }, 500);
 });
 
 $("#events").click(function(){
-	$("html, body").animate({ scrollTop: $('#eventos').offset().top -60 }, 700);
+	$("html, body").animate({ scrollTop: $('#eventos').offset().top -60 }, 500);
 });
 
 $("#portfolio").click(function(){
-	$("html, body").animate({ scrollTop: $('#eventos').offset().top -60 }, 700);
+	$("html, body").animate({ scrollTop: $('#eventos').offset().top -60 }, 500);
 });
 
 $("#servicess").click(function(){
-	$("html, body").animate({ scrollTop: $('#servi').offset().top -70 }, 700);
+	$("html, body").animate({ scrollTop: $('#servi').offset().top -70 }, 500);
 });
 
 // Pagina do curso:
@@ -29,4 +29,8 @@ $("#contat").click(function(){
 
 $("#contatolink").click(function(){
 	$("html, body").animate({ scrollTop: $('#contato').offset().top -100 }, 500);
+});
+
+$("#donationSource").click(function(){
+	$("html, body").animate({ scrollTop: $('#donation').offset().top -100 }, 500);
 });
